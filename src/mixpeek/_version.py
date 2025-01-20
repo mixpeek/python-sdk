9e41ef823976a66d68dd4c3783c6bcd1bc59296d
Ethan Steininger <esteininger21@gmail.com>
2025-01-20 11:39:27 -0800

    c

--- src/mixpeek/_version.py
+++ src/mixpeek/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "mixpeek"
-__version__: str = "0.18.0"
+__version__: str = "0.18.1"
 __openapi_doc_version__: str = "0.81"
 __gen_version__: str = "2.493.32"
-__user_agent__: str = "speakeasy-sdk/python 0.18.0 2.493.32 0.81 mixpeek"
+__user_agent__: str = "speakeasy-sdk/python 0.18.1 2.493.32 0.81 mixpeek"
 
 try:
     if __package__ is not None:
