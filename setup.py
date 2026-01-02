097097678c158ae2e77d27bcb844f8d81abdd670
Ethan Steininger <esteininger21@gmail.com>
2026-01-02 18:22:18 -0500

    chore: bump version to 1.0.0 to supersede existing 0.81 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='mixpeek',
-    version="0.11.4",
+    version="1.0.0",
     author='Ethan Steininger',
     author_email='ethan@mixpeek.com',
     description='Mixpeek Python SDK',
